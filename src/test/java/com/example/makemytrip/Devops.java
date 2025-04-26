e282c532241f88011044ddb182f708b0c368ed61
AkashDake <akashdake9@gmail.com>
2025-04-26 21:02:14 +0530

    java

--- src/test/java/com/example/makemytrip/Devops.java
+++ src/test/java/com/example/makemytrip/Devops.java
@@ -3,5 +3,6 @@ package com.example.makemytrip;
 public class Devops {
     public static void main(String[] args) {
         System.out.println("Hello Demo");
+        System.out.println("Code is working fine");
     }
 }
