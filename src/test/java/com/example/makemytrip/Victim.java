7bdc27092d7f114f60e46f0db0dfc1039cd9b0a9
AkashDake <akashdake9@gmail.com>
2025-05-10 19:13:16 +0530

    A

--- src/test/java/com/example/makemytrip/Victim.java
+++ src/test/java/com/example/makemytrip/Victim.java
@@ -3,5 +3,7 @@ package com.example.makemytrip;
 public class Victim {
     public static void main(String[] args) {
         System.out.println("First Test case");
+        System.out.println("First Test case");
+
     }
 }
